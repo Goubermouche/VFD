b0d2b6032d921fb97c4d6dd1d2034a4a575bd19f
Šimon Tupý <simontupy64@gmail.com>
2022-06-09 13:03:15 +0200

    Added ImGui, glm, GLAD and EnTT

--- FluidEngine/main.cpp
+++ FluidEngine/main.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main()
 {
+    std::cout << "Hello world!" << std::endl;
     return 0;
 }
--- premake5.lua
+++ premake5.lua
@@ -4,16 +4,23 @@ workspace "FluidEngine"
     configurations
     {
         "Debug",
-        "Release",
-        "Dist"
+        "Release"
     }
+
+    startproject "FluidEngine"
 
 outputdir = "{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 -- Include directories relative to root folder (solution directory).
 IncludeDir = {}
-IncludeDir["GLFW"] = "FluidEngine/vendor/GLFW/include"
+IncludeDir["GLFW"] = "FluidEngine/Vendor/GLFW/include"
+IncludeDir["Glad"] = "FluidEngine/Vendor/Glad/include"
+IncludeDir["ImGui"] = "FluidEngine/Vendor/imgui"
+IncludeDir["glm"] = "FluidEngine/Vendor/glm"
+IncludeDir["entt"] = "FluidEngine/Vendor/entt/include"
 
-include "FluidEngine/vendor/GLFW"
+include "FluidEngine/Vendor/GLFW"
+include "FluidEngine/Vendor/Glad"
+include "FluidEngine/Vendor/imgui"
 
 project "FluidEngine"
     location "FluidEngine"
@@ -22,36 +29,60 @@ project "FluidEngine"
 
     targetdir "bin/%{cfg.buildcfg}"
 
+    -- pchheader "pch.h"
+    -- pchsource "FluidEngine/src/pch.cpp"
+
     files
     {
         "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/src/**.cu",
+        "%{prj.name}/src/**.cuh",
+        "%{prj.name}/src/**.txt",
+        "%{prj.name}/src/**.shader",
+
+        -- "%{prj.name}/Vendor/stb/**.h",
+        -- "%{prj.name}/Vendor/stb/**.cpp",
+        "%{prj.name}/Vendor/OBJ-loader/**.h",
+        "%{prj.name}/Vendor/glm/**.hpp",
+        "%{prj.name}/Vendor/glm/**.inl",
     }
 
     includedirs
     {
-        "%{IncludeDir.GLFW}"
+        "%{prj.name}/$(ProjectDir)src",
+        "%{IncludeDir.GLFW}",
+        "%{IncludeDir.Glad}",
+        "%{IncludeDir.ImGui}",
+        "%{IncludeDir.glm}",
+        "%{IncludeDir.entt}",
+        -- "%{prj.name}/$(ProjectDir)Vendor/stb/include",
     }
 
     links
     {
         "GLFW",
+        "Glad",
+        "ImGui",
         "opengl32.lib"
     }
 
     filter "system:windows"
-        cppdialect "C++17"
+        cppdialect "C++20"
         staticruntime "On"
         systemversion "latest"
 
         defines
         {
+            "GLFW_INCLUDE_NONE"
         }
 
     filter "configurations:Debug"
         defines { "DEBUG" }
         symbols "On"
+        staticruntime "off"
   
      filter "configurations:Release"
         defines { "NDEBUG" }
         optimize "On"
+        staticruntime "off"
