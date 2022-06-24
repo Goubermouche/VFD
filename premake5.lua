b2a5d90be4a19c6495c6b2643f8e4c056063c004
Simon Tupý <simontupy64@gmail.com>
2022-06-24 13:41:51 +0200

    Cleaned up the premake5.lua file. Added a profiler hook to the DrawEntityNode function.

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,145 @@
-require('premake5-cuda')
+require("vstudio")
+
+premake.api.register {
+    name = "cudaRelocatableCode",
+    scope = "config",
+    kind = "boolean"
+}
+
+premake.api.register {
+    name = "cudaExtensibleWholeProgram",
+    scope = "config",
+    kind = "boolean"
+}
+
+premake.api.register {
+    name = "cudaCompilerOptions",
+    scope = "config",
+    kind = "table"
+}
+
+premake.api.register {
+    name = "cudaLinkerOptions",
+    scope = "config",
+    kind = "table"
+}
+
+premake.api.register {
+    name = "cudaFastMath",
+    scope = "config",
+    kind = "boolean"
+}
+
+premake.api.register {
+    name = "cudaVerbosePTXAS",
+    scope = "config",
+    kind = "boolean"
+}
+
+premake.api.register {
+    name = "cudaMaxRegCount",
+    scope = "config",
+    kind = "string"
+}
+
+premake.api.register {
+    name = "cudaFiles",
+    scope = "config",
+    kind = "table"
+}
+
+premake.api.register {
+    name = "cudaPath",
+    scope = "config",
+    kind = "string"
+}
+
+local function writeBoolean(property, value)
+    if value == true or value == "On" then
+        premake.w('\t<' .. property .. '>true</' .. property .. '>')
+    elseif value == false or value == "Off" then
+        premake.w('\t<' .. property .. '>false</' .. property .. '>')
+    end
+end
+
+local function writeString(property, value)
+    if value ~= nil and value ~= '' then
+        premake.w('\t<' .. property .. '>' .. value .. '</' .. property .. '>')
+    end
+end
+
+local function writeTableAsOneString(property, values)
+    if values ~= nil then
+        writeString(property, table.concat(values, ' '))
+    end
+end
+
+local function addCompilerProps(cfg)
+    premake.w('<CudaCompile>')
+
+    -- Determine architecture to compile for
+    if cfg.architecture == "x86_64" or cfg.architecture == "x64" then
+        premake.w('\t<TargetMachinePlatform>64</TargetMachinePlatform>')
+    elseif cfg.architecture == "x86" then
+        premake.w('\t<TargetMachinePlatform>32</TargetMachinePlatform>')
+    else
+        error("Unsupported Architecture")
+    end
+
+    -- Set XML tags to their requested values 
+    premake.w('<CodeGeneration></CodeGeneration>')
+    writeBoolean('GenerateRelocatableDeviceCode', cfg.cudaRelocatableCode)
+    writeBoolean('ExtensibleWholeProgramCompilation', cfg.cudaExtensibleWholeProgram)
+    writeBoolean('FastMath', cfg.cudaFastMath)
+    writeBoolean('PtxAsOptionV', cfg.cudaVerbosePTXAS)
+    writeTableAsOneString('AdditionalOptions', cfg.cudaCompilerOptions)
+    writeString('MaxRegCount', cfg.cudaMaxRegCount)
+
+    premake.w('</CudaCompile>')
+end
+
+local function addLinkerProps(cfg)
+    if cfg.cudaLinkerOptions ~= nil then
+        premake.w('<CudaLink>')
+        writeTableAsOneString('AdditionalOptions', cfg.cudaLinkerOptions)
+        premake.w('</CudaLink>')
+    end
+end
+
+premake.override(premake.vstudio.vc2010.elements, "itemDefinitionGroup", function(oldfn, cfg)
+    local items = oldfn(cfg)
+    table.insert(items, addCompilerProps)
+    table.insert(items, addLinkerProps)
+    return items
+end)
+
+local function inlineFileWrite(value)
+    premake.w('\t<CudaCompile ' .. 'Include=' .. string.escapepattern('"') .. path.getabsolute(value) ..
+                  string.escapepattern('"') .. '/>')
+end
+
+local function checkForGlob(value)
+    matchingFiles = os.matchfiles(value)
+    if matchingFiles ~= null then
+        table.foreachi(matchingFiles, inlineFileWrite)
+    end
+end
+
+local function addCUDAFiles(cfg)
+    if cfg.cudaFiles ~= null then
+        premake.w('<ItemGroup>')
+        table.foreachi(cfg.cudaFiles, checkForGlob)
+        premake.w('</ItemGroup>')
+    end
+end
+
+premake.override(premake.vstudio.vc2010.elements, "project", function(oldfn, cfg)
+    local items = oldfn(cfg)
+    table.insert(items, addCUDAFiles)
+    return items
+end)
+
+
 
 workspace "FluidEngine"
     architecture "x64"
@@ -59,7 +200,6 @@ project "FluidEngine"
         "%{prj.name}/src/**.h",
         "%{prj.name}/src/**.cpp",
         "%{prj.name}/src/**.txt",
-        "%{prj.name}/src/**.shader",
         "%{prj.name}/Vendor/glm/**.hpp",
         "%{prj.name}/Vendor/glm/**.inl",
         "%{prj.name}/Vendor/cereal/**.hpp",
