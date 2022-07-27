1469dc90163ef888231a753b0dd929ec309b5e13
Šimon Tupý <simontupy64@gmail.com>
2022-07-27 08:25:25 +0200

    Fixed SPIR-V not working in Debug.

--- premake5.lua
+++ premake5.lua
@@ -99,10 +99,7 @@ workspace "FluidEngine"
 outputdir = "{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 VULKAN_SDK = os.getenv("VULKAN_SDK")
 
--- Include directories relative to root folder (solution directory).
 IncludeDir = {}
-LibraryDir = {}
-Library = {}
 
 IncludeDir["GLFW"] = "FluidEngine/Vendor/GLFW/include"
 IncludeDir["Glad"] = "FluidEngine/Vendor/Glad/include"
@@ -117,14 +114,17 @@ IncludeDir["shaderc"] = "FluidEngine/Vendor/shaderc/include"
 IncludeDir["SPIRV_Cross"] = "FluidEngine/Vendor/SPIRV-Cross"
 IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
 
+LibraryDir = {}
+Library = {}
+
+LibraryDir["VulkanSDK"] = "%{VULKAN_SDK}/Lib"
+
 -- Debug
-LibraryDir["VulkanSDK_Debug"] = "FluidEngine/Vendor/VulkanSDK/Lib"
-Library["ShaderC_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/shaderc_sharedd.lib"
-Library["SPIRV_Cross_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-cored.lib"
-Library["SPIRV_Cross_GLSL_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-glsld.lib"
+Library["ShaderC_Debug"] = "%{LibraryDir.VulkanSDK}/shaderc_sharedd.lib"
+Library["SPIRV_Cross_Debug"] = "%{LibraryDir.VulkanSDK}/spirv-cross-cored.lib"
+Library["SPIRV_Cross_GLSL_Debug"] = "%{LibraryDir.VulkanSDK}/spirv-cross-glsld.lib"
 
 -- Release
-LibraryDir["VulkanSDK"] = "%{VULKAN_SDK}/Lib"
 Library["ShaderC_Release"] = "%{LibraryDir.VulkanSDK}/shaderc_shared.lib"
 Library["SPIRV_Cross_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-core.lib"
 Library["SPIRV_Cross_GLSL_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-glsl.lib"
