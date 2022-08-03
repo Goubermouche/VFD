6bf2fde9b790be35415434ecbe7f41d9449c95e2
Šimon Tupý <simontupy64@gmail.com>
2022-08-03 16:19:56 +0200

    Fixed the model collection scene.

--- Engine/Source/Core/Application.cpp
+++ Engine/Source/Core/Application.cpp
@@ -85,6 +85,14 @@ namespace fe {
 
 		//simulationEntity.AddComponent<SPHSimulationComponent>(simulationDesc);
 
+			// Mesh test
+		//auto meshEntity = m_SceneContext->CreateEntity("bunny");
+		//meshEntity.Transform().Scale = { 3, 3, 3 };
+		//meshEntity.AddComponent<MeshComponent>("Resources/Models/ObjectCollection.obj");
+		//auto& material = meshEntity.AddComponent<MaterialComponent>(Ref<Material>::Create(Renderer::shaderLibrary.GetShader("Resources/Shaders/Normal/BasicDiffuseShader.glsl")));
+		//material.MaterialHandle->Set("color", {0.4f, 0.4f, 0.4f, 1});
+
+
 		Run();
 
 		GPUCompute::Shutdown();
