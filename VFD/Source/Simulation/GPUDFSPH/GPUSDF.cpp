a0a8c916dde9b06d30f358f0ea8ed1a1952c1129
Šimon Tupý <simontupy64@gmail.com>
2022-11-26 11:55:01 +0100

    Cleaned up the SDF CPU implementation.

--- VFD/Source/Simulation/GPUDFSPH/GPUSDF.cpp
+++ VFD/Source/Simulation/GPUDFSPH/GPUSDF.cpp
@@ -335,23 +335,31 @@ namespace vfd {
 
 	float GPUSDF::PointToTriangleDistance(const glm::vec3& x0, const glm::vec3& x1, const glm::vec3& x2, const glm::vec3& x3)
 	{
-		// first find barycentric coordinates of closest point on infinite plane
-		glm::vec3 x13(x1 - x3), x23(x2 - x3), x03(x0 - x3);
-		float m13 = glm::length(x13), m23 = glm::length(x23), d = dot(x13, x23);
-		float invdet = 1.f / max(m13 * m23 - d * d, 1e-30f);
-		float a = dot(x13, x03), b = dot(x23, x03);
-		// the barycentric coordinates themselves
-		float w23 = invdet * (m23 * a - d * b);
-		float w31 = invdet * (m13 * b - d * a);
-		float w12 = 1 - w23 - w31;
-		if (w23 >= 0 && w31 >= 0 && w12 >= 0) {  // if we're inside the triangle
+		const glm::vec3 x13(x1 - x3);
+		const glm::vec3 x23(x2 - x3);
+		const glm::vec3 x03(x0 - x3);
+
+		const float m13 = glm::length(x13);
+		const float m23 = glm::length(x23);
+		const float d = glm::dot(x13, x23);
+		const float invdet = 1.f / max(m13 * m23 - d * d, 1e-30f);
+		const float a = dot(x13, x03);
+		const float b = dot(x23, x03);
+
+		const float w23 = invdet * (m23 * a - d * b);
+		const float w31 = invdet * (m13 * b - d * a);
+		const float w12 = 1.0f - w23 - w31;
+
+		// Inside
+		if (w23 >= 0.0f && w31 >= 0.0f && w12 >= 0.0f) {
 			return glm::distance(x0, w23 * x1 + w31 * x2 + w12 * x3);
 		}
-		else {        // we have to clamp to one of the edges
-			if (w23 > 0) {
+		// Outside?
+		else {
+			if (w23 > 0.0f) {
 				return std::min(PointToSegmentDistance(x0, x1, x2), PointToSegmentDistance(x0, x1, x3));
 			}
-			else if (w31 > 0) {
+			else if (w31 > 0.0f) {
 				return std::min(PointToSegmentDistance(x0, x1, x2), PointToSegmentDistance(x0, x2, x3));
 			}
 			else {
@@ -362,85 +370,145 @@ namespace vfd {
 
 	float GPUSDF::PointToSegmentDistance(const glm::vec3& x0, const glm::vec3& x1, const glm::vec3& x2)
 	{
-		glm::vec3 dx(x2 - x1);
-		double m2 = glm::length(dx);
-		// find parameter value of closest point on segment
-		float s12 = (float)(dot(x2 - x0, dx) / m2);
-		if (s12 < 0) {
-			s12 = 0;
+		const glm::vec3 dx(x2 - x1);
+		const double m2 = glm::length(dx);
+		float s12 = static_cast<float>(dot(x2 - x0, dx) / m2);
+
+		if (s12 < 0.0f) {
+			s12 = 0.0f;
 		}
-		else if (s12 > 1) {
-			s12 = 1;
+		else if (s12 > 1.0f) {
+			s12 = 1.0f;
 		}
 
-		// and find the distance
-		float d = glm::distance(x0, s12 * x1 + (1 - s12) * x2);
-		return d;
+		return glm::distance(x0, s12 * x1 + (1.0f - s12) * x2);
 	}
 
 	bool GPUSDF::PointInTriangle2D(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3, double& a, double& b, double& c)
 	{
-		x1 -= x0; x2 -= x0; x3 -= x0;
-		y1 -= y0; y2 -= y0; y3 -= y0;
-		int signa = Orientation(x2, y2, x3, y3, a);
-		if (signa == 0) return false;
-		int signb = Orientation(x3, y3, x1, y1, b);
-		if (signb != signa) return false;
-		int signc = Orientation(x1, y1, x2, y2, c);
-		if (signc != signa) return false;
-		double sum = a + b + c;
-		assert(sum != 0); // if the SOS signs match and are nonkero, there's no way all of a, b, and c are zero.
+		x1 -= x0;
+		x2 -= x0;
+		x3 -= x0;
+		y1 -= y0;
+		y2 -= y0; 
+		y3 -= y0;
+
+		const int signa = Orientation(x2, y2, x3, y3, a);
+		if (signa == 0) {
+			return false;
+		}
+
+		const int signb = Orientation(x3, y3, x1, y1, b);
+		if (signb != signa) {
+			return false;
+		}
+
+		const int signc = Orientation(x1, y1, x2, y2, c);
+		if (signc != signa) {
+			return false;
+		}
+
+		const double sum = a + b + c;
+
 		a /= sum;
 		b /= sum;
 		c /= sum;
+
 		return true;
 	}
 
 	int GPUSDF::Orientation(double x1, double y1, double x2, double y2, double& twice_signed_area)
 	{
 		twice_signed_area = y1 * x2 - x1 * y2;
-		if (twice_signed_area > 0) return 1;
-		else if (twice_signed_area < 0) return -1;
-		else if (y2 > y1) return 1;
-		else if (y2 < y1) return -1;
-		else if (x1 > x2) return 1;
-		else if (x1 < x2) return -1;
-		else return 0; // only true when x1==x2 and y1==y2
+
+		if (twice_signed_area > 0.0) {
+			return 1;
+		}
+		else if (twice_signed_area < 0.0) {
+			return -1;
+		}
+		else if (y2 > y1) {
+			return 1;
+		}
+		else if (y2 < y1) {
+			return -1;
+		}
+		else if (x1 > x2) {
+			return 1;
+		}
+		else if (x1 < x2) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
 	}
 
 	void GPUSDF::Sweep(const std::vector<glm::uvec3>& tri, const std::vector<glm::vec3>& x, Array3f& phi, Array3i& closest_tri, const glm::vec3& origin, float dx, int di, int dj, int dk)
 	{
-		int i0, i1;
-		if (di > 0) { i0 = 1; i1 = phi.ni; }
-		else { i0 = phi.ni - 2; i1 = -1; }
-		int j0, j1;
-		if (dj > 0) { j0 = 1; j1 = phi.nj; }
-		else { j0 = phi.nj - 2; j1 = -1; }
-		int k0, k1;
-		if (dk > 0) { k0 = 1; k1 = phi.nk; }
-		else { k0 = phi.nk - 2; k1 = -1; }
-		for (int k = k0; k != k1; k += dk) for (int j = j0; j != j1; j += dj) for (int i = i0; i != i1; i += di) {
-			glm::vec3 gx(i * dx + origin[0], j * dx + origin[1], k * dx + origin[2]);
-			CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i - di, j, k);
-			CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i, j - dj, k);
-			CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i - di, j - dj, k);
-			CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i, j, k - dk);
-			CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i - di, j, k - dk);
-			CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i, j - dj, k - dk);
-			CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i - di, j - dj, k - dk);
+		int i0;
+		int i1;
+
+		if (di > 0) {
+			i0 = 1; 
+			i1 = phi.ni;
+		}
+		else {
+			i0 = phi.ni - 2; 
+			i1 = -1;
+		}
+
+		int j0;
+		int j1;
+
+		if (dj > 0) { 
+			j0 = 1;
+			j1 = phi.nj; 
+		}
+		else { 
+			j0 = phi.nj - 2;
+			j1 = -1;
+		}
+
+		int k0;
+		int k1;
+
+		if (dk > 0) {
+			k0 = 1; 
+			k1 = phi.nk;
+		}
+		else {
+			k0 = phi.nk - 2;
+			k1 = -1; 
+		}
+
+		for (int k = k0; k != k1; k += dk) {
+			for (int j = j0; j != j1; j += dj) {
+				for (int i = i0; i != i1; i += di) {
+					const glm::vec3 gx(i * dx + origin[0], j * dx + origin[1], k * dx + origin[2]);
+
+					CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i - di, j     , k     );
+					CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i     , j - dj, k     );
+					CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i - di, j - dj, k     );
+					CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i     , j     , k - dk);
+					CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i - di, j     , k - dk);
+					CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i     , j - dj, k - dk);
+					CheckNeighbor(tri, x, phi, closest_tri, gx, i, j, k, i - di, j - dj, k - dk);
+				}
+			}
 		}
 	}
 
 	void GPUSDF::CheckNeighbor(const std::vector<glm::uvec3>& tri, const std::vector<glm::vec3>& x, Array3f& phi, Array3i& closest_tri, const glm::vec3& gx, int i0, int j0, int k0, int i1, int j1, int k1)
 	{
 		if (closest_tri(i1, j1, k1) >= 0) {
+			const glm::vec3 v = tri[closest_tri(i1, j1, k1)];
+			const unsigned int p = v.x;
+			const unsigned int q = v.y;
+			const unsigned int r = v.z;
 
-			auto v = tri[closest_tri(i1, j1, k1)];
-			unsigned int p = v.x;
-			unsigned int q = v.y;
-			unsigned int r = v.z;
+			const float d = PointToTriangleDistance(gx, x[p], x[q], x[r]);
 
-			float d = PointToTriangleDistance(gx, x[p], x[q], x[r]);
 			if (d < phi(i0, j0, k0)) {
 				phi(i0, j0, k0) = d;
 				closest_tri(i0, j0, k0) = closest_tri(i1, j1, k1);
