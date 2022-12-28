4154870a0e3f7574258e828e35c3e3ea345bdfcd
Šimon Tupý <simontupy64@gmail.com>
2022-12-28 14:10:50 +0100

    Clenaed up the precomputed cubic kernel class.

--- VFD/Source/Simulation/GPUDFSPH/Kernel/DFSPHKernels.h
+++ VFD/Source/Simulation/GPUDFSPH/Kernel/DFSPHKernels.h
@@ -17,19 +17,19 @@ namespace vfd {
 			m_Radius2 = m_Radius * m_Radius;
 			const float radius3 = m_Radius * m_Radius * m_Radius;
 
-			m_K = static_cast<float>(8.0) / (pi * radius3);
-			m_L = static_cast<float>(48.0) / (pi * radius3);
+			m_K = 8.0f / (pi * radius3);
+			m_L = 48.0f / (pi * radius3);
 
 			m_WZero = CalculateW({0.0f, 0.0f, 0.0f});
 
 			const float stepSize = m_Radius / static_cast<float>(Resolution - 1);
-			m_InvStepSize = static_cast<float>(1.0) / stepSize;
+			m_InvStepSize = 1.0f / stepSize;
 
 			for (unsigned int i = 0; i < Resolution; i++) {
 				const float posX = stepSize * static_cast<float>(i);
 				m_W[i] = CalculateW(posX);
 
-				if (posX > 1.0e-9) {
+				if (posX > 1.0e-9f) {
 					m_GradientW[i] = CalculateGradientW({ posX, 0.0f, 0.0f }).x / posX;
 				}
 				else {
@@ -45,20 +45,20 @@ namespace vfd {
 
 			if (r <= m_Radius) {
 				const unsigned int pos = glm::min(static_cast<unsigned int>(r * m_InvStepSize), Resolution - 2u);
-				res = static_cast<float>(0.5) * (m_W[pos] + m_W[pos + 1]);
+				res = 0.5f * (m_W[pos] + m_W[pos + 1]);
 			}
 
 			return res;
 		}
 
 		__host__ __device__ float GetW(const glm::vec3& r) {
-			float res = 0.0;
+			float res = 0.0f;
 			const float r2 = dot(r, r);
 
 			if (r2 <= m_Radius2) {
 				const float rl = sqrt(r2);
 				const unsigned int pos = glm::min(static_cast<unsigned int>(rl * m_InvStepSize), Resolution - 2u);
-				res = static_cast<float>(0.5) * (m_W[pos] + m_W[pos + 1]);
+				res = 0.5f * (m_W[pos] + m_W[pos + 1]);
 			}
 
 			return res;
@@ -70,7 +70,7 @@ namespace vfd {
 
 			if (rl <= m_Radius) {
 				const unsigned int pos = glm::min(static_cast<unsigned int>(rl * m_InvStepSize), Resolution - 2u);
-				res = static_cast<float>(0.5) * (m_GradientW[pos] + m_GradientW[pos + 1]) * r;
+				res = 0.5f * (m_GradientW[pos] + m_GradientW[pos + 1]) * r;
 			}
 			else {
 				res = { 0.0f, 0.0f, 0.0f };
@@ -79,46 +79,48 @@ namespace vfd {
 			return res;
 		}
 
-		__host__ __device__ float GetWZero() {
+		__host__ __device__ float GetWZero() const {
 			return m_WZero;
 		}
 	private:
-		__host__ __device__ float CalculateW(const float r) {
-			float res = 0.0;
+		__host__ __device__ float CalculateW(const float r) const {
+			float res = 0.0f;
 			const float q = r / m_Radius;
 
 			if (q <= 1.0f) {
 				if (q <= 0.5f) {
 					const float q2 = q * q;
 					const float q3 = q2 * q;
-					res = m_K * (static_cast<float>(6.0) * q3 - static_cast<float>(6.0) * q2 + static_cast<float>(1.0));
+					res = m_K * (6.0f * q3 - 6.0f * q2 + 1.0f);
 				}
 				else {
-					res = m_K * (static_cast<float>(2.0) * pow(static_cast<float>(1.0) - q, static_cast<float>(3.0)));
+					const float q1 = 1.0f - q;
+					const float q3 = q1 * q1 * q1;
+					res = m_K * (2.0f * q3);
 				}
 			}
 
 			return res;
 		}
 
-		__host__ __device__ float CalculateW(const glm::vec3& r) {
+		__host__ __device__ float CalculateW(const glm::vec3& r) const {
 			return CalculateW(sqrt(dot(r, r)));
 		}
 
-		__host__ __device__ glm::vec3 CalculateGradientW(const glm::vec3& r) {
+		__host__ __device__ glm::vec3 CalculateGradientW(const glm::vec3& r) const {
 			glm::vec3 res;
 			const float rl = sqrt(dot(r, r));
 			const float q = rl / m_Radius;
-			if (rl > 1.0e-5 && (q <= 1.0f))
+			if (rl > 1.0e-5f && (q <= 1.0f))
 			{
-				const glm::vec3 gradientQ = r * (static_cast<float>(1.0) / (rl * m_Radius));
+				const glm::vec3 gradientQ = r * (1.0f / (rl * m_Radius));
 				if (q <= 0.5f)
 				{
-					res = m_L * q * ((float)3.0 * q - static_cast<float>(2.0)) * gradientQ;
+					res = m_L * q * (3.0f * q - 2.0f) * gradientQ;
 				}
 				else
 				{
-					const float factor = static_cast<float>(1.0) - q;
+					const float factor = 1.0f - q;
 					res = m_L * (-factor * factor) * gradientQ;
 				}
 			}
@@ -134,7 +136,6 @@ namespace vfd {
 		float m_Radius2;
 		float m_InvStepSize;
 		float m_WZero;
-
 		float m_K;
 		float m_L;
 	};
